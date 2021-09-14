589472ce8597cb20d012b2bdf13ecec803200092
John Soares <john.soares@cwi.com.br>
2021-09-14 06:58:15 -0300

    Inicio do desenvolvimento das validações da entidade produto.

---
+++ src/NerdStore.Catalog.Domain/Product.cs
@@ -0,0 +1,99 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Catalog.Domain
+{
+    public class Product : Entity, IAggregateRoot
+    {
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        public bool IsActive { get; private set; }
+
+        public decimal Value { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+
+        public File Image { get; private set; }
+
+        public int AmountStock { get; private set; }
+
+        public Guid CategoryId { get; private set; }
+
+        public Category Category { get; private set; }
+
+        public Product(string name, string description, bool isActive, decimal value, DateTime createdAt, File image, Guid categoryId)
+        {
+            Name = name;
+            Description = description;
+            IsActive = isActive;
+            Value = value;
+            CreatedAt = createdAt;
+            Image = image;
+            CategoryId = categoryId;
+        }
+
+        public void Activate() => IsActive = true;
+
+        public void Deactivate() => IsActive = false;
+
+        public void ChangeCategory(Category category)
+        {
+            Category = category;
+            CategoryId = category.Id;
+        }
+
+        public void ChangeDescription(string description)
+        {
+            Description = description;
+        }
+
+        public void DebitStock(int amount)
+        {
+            if (amount < 0) amount *= -1;
+            AmountStock -= amount;
+        }
+
+        public void ReplenishStock(int amount)
+        {
+            AmountStock += amount;
+        }
+
+        public bool HaveStock(int amount)
+        {
+            return AmountStock >= amount;
+        }
+
+        public void Validate()
+        {
+        }
+    }
+
+    public class Category : Entity
+    {
+        public string Name { get; private set; }
+
+        public int Code { get; private set; }
+
+        public Category(string name, int code)
+        {
+            Name = name;
+            Code = code;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Code}";
+        }
+    }
+
+    public class File : Entity
+    {
+        public string FileName { get; set; }
+
+        public byte[] DataFiles { get; set; }
+    }
+}
---
+++ src/NerdStore.Core/DomainObjects/Validations.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NerdStore.Core.DomainObjects
+{
+    //AssertConcern Vernon's Tip
+    public class Validations
+    {
+        public static void ValidateIfEqual(object object1, object object2, string message)
+        {
+            if (!object1.Equals(object2))
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfDifferent(object object1, object object2, string message)
+        {
+            if (object1.Equals(object2))
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateCharacters(string value, int maximum, string message)
+        {
+            if(value.Trim().Length > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateCharacters(string value, int minimum, int maximum, string message)
+        {
+            int length = value.Trim().Length;
+            if(length < minimum || length > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateExpression(string pattern, string value, string message)
+        {
+            var regex = new Regex(pattern);
+
+            if (!regex.IsMatch(value))
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfEmpty(string value, string message)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfNull(object object1, string message)
+        {
+            if(object1 == null)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateMinimumMaximum(double value, double minimum, double maximum, string message)
+        {
+            if(value < minimum || value > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateMinimumMaximum(float value, float minimum, float maximum, string message)
+        {
+            if (value < minimum || value > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateMinimumMaximum(int value, int minimum, int maximum, string message)
+        {
+            if (value < minimum || value > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateMinimumMaximum(long value, long minimum, long maximum, string message)
+        {
+            if (value < minimum || value > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateMinimumMaximum(decimal value, decimal minimum, decimal maximum, string message)
+        {
+            if (value < minimum || value > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfMinorEqualMinimum(long value, long minimum, string message)
+        {
+            if (value <= minimum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfMinorEqualMinimum(double value, double minimum, string message)
+        {
+            if (value <= minimum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfMinorEqualMinimum(decimal value, decimal minimum, string message)
+        {
+            if (value <= minimum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfMinorEqualMinimum(int value, int minimum, string message)
+        {
+            if (value <= minimum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfMinorEqualMinimum(float value, float minimum, string message)
+        {
+            if (value <= minimum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfFalse(bool value, string message)
+        {
+            if (value)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfTrue(bool value, string message)
+        {
+            if (!value)
+            {
+                throw new DomainException(message);
+            }
+        }
+    }
+}
